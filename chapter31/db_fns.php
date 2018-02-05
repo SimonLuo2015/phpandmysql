98fdcd311eab2cc668b7b4f196ae1c7823ba3731
SimonLuo2015 <luoshujian1987@163.com>
2018-02-05 20:55:50 +0800

    function for connect to mysql

---
+++ chapter31/db_fns.php
@@ -0,0 +1,9 @@
+<?php
+    function db_connect(){
+        $result = new mysqli('localhost', 'discussion', 'password', 'discussion');
+        if (!$result){
+            return false;
+        }
+        return $result;
+    }
+?>
