530fcacaf7977c8cdbfc9821745bff3ee5df2892
SimonLuo2015 <luoshujian1987@163.com>
2018-02-06 14:28:37 +0800

    update class codes.

--- chapter31/treenode_class.php
+++ chapter31/treenode_class.php
@@ -31,9 +31,30 @@ class treenode{
             $query = "select * from header where parent=$postid order by posted";
             $result = $conn->query($query);
             for ($count=0; $row=@$result->fetch_assoc(); $count++){
-                
+                if ($sublist || @$expanded[$row['postid']]==true){
+                    $expand = true;
+                }else{
+                    $expand = false;
+                }
+                $this->m_childlist[$count] = new treenode($row['postid'], $row['title'], $row['poster'], $row['posted'], $row['children'], $expand, $depth+1, $expanded, $sublist);
             }
         }                
+
+    }
+
+    function display($row, $sublist=false){
+        // as this is an object, it is responsible for displaying itself.
+        // $row tells us what row of the display we are up to, 
+        // so we know what color it should be.
+        // $sublist tells us whether we are on the main page
+        // or the message page. Message pages should have $sublist=true.
+        // On a sublist, all messages are expanded and there are no "+" or "-" symbols.
+
+        // If this is the empty root node, skip displaying.
+        if ($this->m_depth > -1){
+            
+        }
+
 
     }
 }
